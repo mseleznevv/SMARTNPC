1b72f15a8f651f7ec29bf4a593118ebb3dae15bc
mseleznevdev <mseleznevdev@gmail.com>
2026-04-25 01:02:16 +0300

    Добавил движение персонажа и настроил управление камерой мышкой

--- player.gd
+++ player.gd
@@ -1,32 +1,41 @@
 extends CharacterBody3D
 
 @export var speed = 7
-@export var fall_acceleration = 75
+@export var gravity = 9.8
+@export var mouse_sensitivity = 0.002
 
-var target_velocity = Vector3.ZERO
+var direction = transform.basis
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-		
-	if direction != Vector3.ZERO:
+	if input_dir != Vector2.ZERO:
+		direction = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
 		direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction)
-		$CameraPivotPlayer.basis = Basis.looking_at(direction)
-		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-
+	
+		print(transform.basis, " : ", input_dir, " : ", direction)
+	
+		#$Pivot.basis = Basis.looking_at(direction)
+	
+	if input_dir != Vector2.ZERO:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
 	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration*delta)
+		velocity.y -= gravity * delta
 		
-	velocity = target_velocity
 	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
